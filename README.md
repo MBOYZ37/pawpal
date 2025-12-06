d3f711773b4e2dba2dbdf9d80d8268f3fd03fa4e
Haziq Haikal <haziqhaikal2605@gmail.com>
2025-12-06 16:53:09 +0800

    Add pet submission feature with image upload and geolocation
    - Implemented Pet model to handle pet data.
    - Created SubmitPetScreen for users to submit pet details, including images and location.
    - Integrated image picker and cropper for selecting and editing pet images.
    - Added geolocation functionality to fetch user's current location.
    - Developed API endpoints for saving and retrieving pet data from the server.
    - Updated pubspec.yaml to include necessary dependencies (geolocator, image_cropper, image_picker).
    - Registered new plugins for file selection and geolocation on Linux, macOS, and Windows.
    - Enhanced error handling and user feedback during pet submission process.

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
 # pawpal
 
 Pet Adoption & Donation App
+
+## Table of Contents
+- [Setup](#setup)
+- [API Explainantion](#api-explainantion)
+- [Sample JSON Response](#sample-json-response)
+
+## Setup
+
+### 1. Install the dependencies
+To install the dependencies, run the following command:
+
+```bash
+flutter pub get
+```
+### 2. Install Xampp
+### 3. Import the database pawpal_db.sql into Xampp
+### 4. Copy API folder to Xampp
+### 5. Copy uploads folder to Xampp
+### 6. Change IP Address at myconfig.dart
+### 5. Run the main.dart file
+
+
+## API Explainantion
+### 1. dbconnect.php
+to handle database connection
+
+### 2. login.php
+to handle user login and authentication 
+
+### 3. register_user.php    
+to handle user registration and ecryption of password and check email already registered or not.
+
+### 4. get_my_pets.php
+to load everything about pets and load pets based on search query
+
+### 5. submit_pets.php
+to handle submissions of the pets and images of the pets
+
+
+## Sample JSON Response
+### Example of JSON Response for get_my_pets.php
+#### ✅ 1. Success Response (With Data)
+```json
+{
+  "status": "success",
+  "data": [
+    {
+      "pet_id": "3",
+      "user_id": "7",
+      "pet_name": "Oyen",
+      "pet_type": "Cat",
+      "category": "Lost",
+      "description": "Cute and have black neck collar with engrave named 'Oyen'",
+      "images_path": "uploads/pets_12_1.png,uploads/pets_12_2.png",
+      "lat": "6.4469995",
+      "lng": "100.5116055",
+      "created_at": "2025-12-06 16:37:19",
+      "name": "Haziq",
+      "email": "haziqhaikal@gmail.com",
+      "phone": "0111111111",
+      "reg_date": "2024-12-01 11:37:05"
+    }
+  ]
+}
+```    
+#### ✅ 2. Success Response (But No Results Found)
+```json
+{
+    
+  "status": "failed",
+  "data": []
+}
+```
+### Example of JSON Response for submit_pets.php
+#### ✅ 1. Success Response
+```json
+{
+  "status": "success",
+  "message": "Pet added successfully"
+}
+```
+#### ❌ 2. Failed Response
+```json
+{
+  "status": "failed",
+  "message": "No images provided"
+}
+```
--- lib/views/homescreen.dart
+++ lib/views/homescreen.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:pawpal/views/loginscreen.dart';
+import 'package:http/http.dart' as http;
+import 'package:pawpal/models/pet.dart';
 import 'package:pawpal/models/user.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:pawpal/myconfig.dart';
+import 'package:pawpal/views/loginscreen.dart';
+import 'package:pawpal/views/submitpetscreen.dart';
 
 class HomeScreen extends StatefulWidget {
   final User? user;
@@ -12,47 +17,338 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Pet> petList = [];
+  String status = "Loading...";
+  late double screenWidth, screenHeight;
+
+  @override
+  void initState() {
+    super.initState();
+    loadPets('');
+  }
+
   @override
   Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+    if (screenWidth > 600) {
+      screenWidth = 600;
+    } else {
+      screenWidth = screenWidth;
+    }
+
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 177, 177, 177),
-        title: const Text('PawPal', style: TextStyle(color: Colors.white)),
-      ),
-      backgroundColor: Color.fromARGB(255, 255, 235, 227),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome, ${widget.user?.userName ?? 'User'}!'),
-            SizedBox(height: 20),
-            Text('User ID: ${widget.user?.userId ?? 'Not provided'}'),
-            SizedBox(height: 20),
-            Text('Email: ${widget.user?.userEmail ?? 'Not provided'}'),
-            SizedBox(height: 20),
-            Text('Phone: ${widget.user?.userPhone ?? 'Not provided'}'),
-            SizedBox(height: 20),
-            Text('Registered on: ${widget.user?.userRegDate ?? 'Not provided'}'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                signOut();
-              }, 
-              child: Text('Sign Out')),
-          ],
+        title: Text(
+          'Paw Pal',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
+        backgroundColor: const Color.fromARGB(255, 177, 177, 177),
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearchDialog();
+            },
+          ),
+          IconButton(
+            onPressed: () {
+              loadPets('');
+            },
+            icon: Icon(Icons.refresh),
+          ),
+
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LogInScreen()),
+              );
+            },
+            icon: Icon(Icons.login),
+          ),
+        ],
+      ),
+      body: Center(
+        child: SizedBox(
+          width: screenWidth,
+          child: Column(
+            children: [
+              petList.isEmpty
+                  ? Expanded(
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.find_in_page_outlined, size: 64),
+                            SizedBox(height: 12),
+                            Text(
+                              status,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: petList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            color: Colors.cyan[100],
+                            elevation: 4,
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 6,
+                              horizontal: 8,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // IMAGE
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Container(
+                                      width:
+                                          screenWidth * 0.28, // more responsive
+                                      height:
+                                          screenWidth *
+                                          0.22, // balanced aspect ratio
+                                      color: Colors.grey[200],
+                                      child: Image.network(
+                                        // path to retrieve image from server
+                                        '${MyConfig.baseUrl}/pawpal/${petList[index].imagesPath[0]}',
+                                        fit: BoxFit.cover,
+                                        errorBuilder:
+                                            (context, error, stackTrace) {
+                                              return const Icon(
+                                                Icons.broken_image,
+                                                size: 60,
+                                                color: Colors.grey,
+                                              );
+                                            },
+                                      ),
+                                    ),
+                                  ),
+
+                                  const SizedBox(width: 12),
+
+                                  // TEXT AREA
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        // TITLE
+                                        Text(
+                                          petList[index].petName.toString(),
+                                          style: const TextStyle(
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+
+                                        const SizedBox(height: 4),
+
+                                        // DESCRIPTION
+                                        Text(
+                                          petList[index].description.toString(),
+                                          style: const TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.black87,
+                                          ),
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+
+                                        const SizedBox(height: 6),
+
+                                        Row(
+                                          children: [
+                                            // Category Badge
+                                            Container(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                    horizontal: 8,
+                                                    vertical: 4,
+                                                  ),
+                                              decoration: BoxDecoration(
+                                                color: Colors.black.withOpacity(
+                                                  0.15,
+                                                ),
+                                                borderRadius:
+                                                    BorderRadius.circular(8),
+                                              ),
+                                              child: Text(
+                                                petList[index].category
+                                                    .toString(),
+                                                style: const TextStyle(
+                                                  fontSize: 15,
+                                                  color: Color.fromARGB(
+                                                    255,
+                                                    1,
+                                                    1,
+                                                    1,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+
+                                            const SizedBox(width: 10),
+
+                                            // Pet Type Badge
+                                            Container(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                    horizontal: 8,
+                                                    vertical: 4,
+                                                  ),
+                                              decoration: BoxDecoration(
+                                                color: Colors.black.withOpacity(
+                                                  0.15,
+                                                ),
+                                                borderRadius:
+                                                    BorderRadius.circular(8),
+                                              ),
+                                              child: Text(
+                                                petList[index].petType
+                                                    .toString(),
+                                                style: const TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.black,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // Action for the button
+          if (widget.user?.userId == '0') {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Please login first/or register first"),
+                backgroundColor: Colors.red,
+              ),
+            );
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LogInScreen()),
+            );
+          } else {
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SubmitPetScreen(user: widget.user),
+              ),
+            );
+            loadPets('');
+          }
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
 
-  void signOut() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.clear();
-    if (!mounted) return;
-    Navigator.pop(context);
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => LogInScreen())
+  void showSearchDialog() {
+    TextEditingController searchController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Search'),
+          content: TextField(
+            controller: searchController,
+            decoration: InputDecoration(hintText: 'Enter search query'),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Search'),
+              onPressed: () {
+                String search = searchController.text;
+                if (search.isEmpty) {
+                  loadPets('');
+                } else {
+                  loadPets(search);
+                }
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  void loadPets(String searchQuery) {
+    petList.clear();
+    setState(() {
+      status = "Loading...";
+    });
+
+    http
+        .get(
+          Uri.parse(
+            //api path to get pets
+            '${MyConfig.baseUrl}/pawpal/API/get_my_pets.php?search=$searchQuery',
+          ),
+        )
+        .then((response) {
+          if (response.statusCode == 200) {
+            var jsonResponse = jsonDecode(response.body);
+
+            if (jsonResponse['status'] == 'success' &&
+                jsonResponse['data'] != null &&
+                jsonResponse['data'].isNotEmpty) {
+              petList.clear();
+              for (var item in jsonResponse['data']) {
+                petList.add(Pet.fromJson(item));
+              }
+
+              setState(() {
+                status = "";
+              });
+            } else {
+              setState(() {
+                petList.clear();
+                status = "No submissions yet";
+              });
+            }
+          } else {
+            setState(() {
+              petList.clear();
+              status = "Failed to load list of pets";
+            });
+          }
+        });
   }
 }
