aafe11dc8f19c2f602df8ba0de846f0db2c712a8
Haziq Haikal <haziqhaikal2605@gmail.com>
2025-12-06 14:12:03 +0800

    fix: update navigation in SplashScreen to redirect to HomeScreen for Mid Term

--- lib/views/splashscreen.dart
+++ lib/views/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pawpal/views/homescreen.dart';
 import 'package:pawpal/views/loginscreen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
       if (mounted){
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const LogInScreen()),
+        MaterialPageRoute(builder: (context) => const HomeScreen(user: null,)),
       );
       }
     });
